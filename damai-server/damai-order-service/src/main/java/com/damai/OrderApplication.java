aadac54b32fe9d219efa5455c7c77f54be0d1854
cloris888 <2981872460@qq.com>
2025-04-05 19:12:16 +0800

    完整项目初次推送

--- damai-server/damai-order-service/src/main/java/com/damai/OrderApplication.java
+++ damai-server/damai-order-service/src/main/java/com/damai/OrderApplication.java
@@ -14,7 +14,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
-public class OrderApplication {
+public class
+OrderApplication {
 
     public static void main(String[] args) {
         System.setProperty("nacos.logging.default.config.enabled","false");
